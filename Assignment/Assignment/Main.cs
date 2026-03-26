1f5c8f13b19b8e257e23617910140f1167d9f62c
Arun Prasaath <arunpra@infinite.com>
2026-03-26 15:23:39 +0530

    samplefileadded

---
+++ Assignment/Assignment/Main.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            
+            EqualorNot.Isequal();
+            PosorNeg.Pos();
+            arithmetic.operations();
+            multiply.table();
+            sum.twointeger();
+        }
+    }
+}
