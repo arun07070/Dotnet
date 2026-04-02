59765477dd89e07bc4d9834e7033ac218872dc24
Arun Prasaath <arunpra@infinite.com>
2026-04-02 11:50:41 +0530

    Code Challenge 1 completed

---
+++ Code Challenge 1/Code Challenge 1/NestedStructure.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Challenge_1
+{
+    struct DateOfBirth
+    {
+        public int Day;
+        public int Month;
+        public int Year;
+    }
+    class employees
+    {
+        public string Name;
+        public DateOfBirth Dob;
+        public void Display()
+        {
+            Console.WriteLine("\nEmployee Name: " + Name);
+            Console.WriteLine("Date of Birth: " + Dob.Day + "/" + Dob.Month + "/" + Dob.Year);
+        }
+    }
+    class pro
+    {
+        static void Main(string[] args)
+        {
+            employees[] emp = new employees[2];
+            for (int i = 0; i < emp.Length; i++)
+            {
+                emp[i] = new employees();
+                Console.WriteLine("\nEnter details for Employee " + (i + 1));
+                Console.Write("Name of the employee: ");
+                emp[i].Name = Console.ReadLine();
+                Console.Write("Input day of the birth: ");
+                int day = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Input month of the birth: ");
+                int month = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Input year of the birth: ");
+                int year = Convert.ToInt32(Console.ReadLine());
+                emp[i].Dob = new DateOfBirth
+                {
+                    Day = day,
+                    Month = month,
+                    Year = year
+                };
+            }
+            Console.WriteLine("\n--- Employee Details ---");
+            foreach (employees e in emp)
+            {
+                e.Display();
+            }
+        }
+    }
+}
---
+++ Code Challenge 1/Code Challenge 1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Challenge_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeService service = new EmployeeService();
+            int choice;
+
+            do
+            {
+                Console.WriteLine("\n===== Employee Management Menu =====");
+                Console.WriteLine("1. Add New Employee");
+                Console.WriteLine("2. View All Employees");
+                Console.WriteLine("3. Search Employee by ID");
+                Console.WriteLine("4. Update Employee Details");
+                Console.WriteLine("5. Delete Employee");
+                Console.WriteLine("6. Exit");
+                Console.WriteLine("====================================");
+                Console.Write("Enter your choice: ");
+
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        service.AddEmployee();
+                        break;
+
+                    case 2:
+                        service.ViewEmployees();
+                        break;
+
+                    case 3:
+                        service.SearchEmployee();
+                        break;
+
+                    case 4:
+                        service.UpdateEmployee();
+                        break;
+
+                    case 5:
+                        service.DeleteEmployee();
+                        break;
+
+                    case 6:
+                        Console.WriteLine("Exiting...");
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid choice!");
+                        break;
+                }
+
+            } while (choice != 6);
+        }
+    }
+}
