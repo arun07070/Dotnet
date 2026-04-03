bb8d9d7ceceea80bd0dc9117848ba72e751a56ec
Arun Prasaath <arunpra@infinite.com>
2026-04-03 11:51:59 +0530

    Assignment 4 added

---
+++ Assignment4/Assignment4/Remove.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    internal class Remove
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            string str = Console.ReadLine();
+            Console.Write("Enter position to remove: ");
+            int pos = int.Parse(Console.ReadLine());
+            if (pos >= 0 && pos < str.Length)
+            {
+                string result = str.Remove(pos, 1);
+                Console.WriteLine("Result: " + result);
+            }
+            else
+            {
+                Console.WriteLine("Invalid position!");
+            }
+        }
+    }
+}
