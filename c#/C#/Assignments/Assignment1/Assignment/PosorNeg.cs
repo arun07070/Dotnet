cbe646f5466a224a44b09cde5f57474160d370f1
Arun Prasaath <arunpra@infinite.com>
2026-04-08 10:25:30 +0530

    Reorganized All the Folders

---
+++ c#/C#/Assignments/Assignment1/Assignment/PosorNeg.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class PosorNeg
+    {
+        public static void Pos()
+        {
+            Console.Write("Enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num > 0)
+            {
+                Console.WriteLine(num + " is a positive number");
+            }
+            else if (num < 0)
+            {
+                Console.WriteLine(num + " is a negative number");
+            }
+            else
+            {
+                Console.WriteLine("The number is zero");
+            }
+        }
+    }
+}
---
+++ c#/C#/Assignments/Assignment2/length.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assi2
+{
+    internal class length
+    {
+        public static void Stringlen()
+        {
+            Console.Write("Enter a word: ");
+            string word = Console.ReadLine();
+
+            Console.WriteLine("Length: " + word.Length);
+        }
+    }
+}
