dbb5a8a4275f467445b18400872dd2c5c0006524
Arun Prasaath <arunpra@infinite.com>
2026-04-16 11:04:22 +0530

    Code Challenge 3 Completed

---
+++ c#/C#/Assessments/Code Challenge 3/Code Challenge 3/Append.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Challenge_3
+{
+    internal class Append
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\dotnet\c#\C#\Assessments\Code Challenge 3\Code Challenge 3\TextAppend.txt";
+            Console.WriteLine("Enter text to append to the file:");
+            string text = Console.ReadLine();
+            using (StreamWriter writer = new StreamWriter(filePath, true))
+            {
+                writer.WriteLine(text);
+            }
+            Console.WriteLine("Text appended successfully.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ c#/C#/Assessments/Code Challenge 3/Code Challenge 3/ipl.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Challenge_3
+{
+    class CricketTeam
+    {
+        public (int count, int sum, double average) Pointscalculation(int no_of_matches)
+        {
+            int[] scores = new int[no_of_matches];
+            int sum = 0;
+            for (int i = 0; i < no_of_matches; i++)
+            {
+                Console.Write($"Enter score for match {i + 1}: ");
+                scores[i] = Convert.ToInt32(Console.ReadLine());
+                sum += scores[i];
+            }
+            double average = (double)sum / no_of_matches;
+            return (no_of_matches, sum, average);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter number of matches: ");
+            int matches = Convert.ToInt32(Console.ReadLine());
+            CricketTeam team = new CricketTeam();
+            var result = team.Pointscalculation(matches);
+            Console.WriteLine("\n--- Result ---");
+            Console.WriteLine("Total Matches: " + result.count);
+            Console.WriteLine("Sum of Scores: " + result.sum);
+            Console.WriteLine("Average Score: " + result.average);
+        }
+    }
+}
